640098515b1e7556c3bc7cdb093672e5d3be6188
Malinna Leach <malinnab@yahoo.com>
2016-10-10 17:23:53 +0100

    adding post

--- app.rb
+++ app.rb
@@ -17,8 +17,13 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params[:name]
   @name = params[:name]
   erb(:index)
+end
+
+get '/name-the-cat' do
+  p params[:name]
+  erb(:form)
 end
